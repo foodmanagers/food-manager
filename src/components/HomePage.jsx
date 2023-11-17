f06b752a69c6cef6d9c49d206b44d35ca6420ba0
Hugo Pereira <hugocastroferro@gmail.com>
2023-11-17 12:28:09 +0000

    [Add] First HomePage css

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,35 @@
+import "./HomePage.css"
+
 function HomePage () {
     return(
-        <div>
-            <h1>HomePage HERE</h1>
+        <div className="homepagebox">
+        <section className="food-manager-app">
+            <div className="app-title">
+            <h1>Food Manager App</h1>
+            <p>Efficiently manage your corporate food needs</p>
+            </div>
+            </section>
+
+            <section className="function-boxes">
+            <div className="little-box">
+            <h2>Catalog</h2>
+            <p>Elegant menu display for clients</p>
+            </div>
+            <div className="little-box">
+            <h2>Details Page</h2>
+            <p>Informative page where clients can see Menu details</p>
+            </div>
+            <div className="little-box">
+            <h2>Menu Management</h2>
+            <p>Interactive Menu to create, edit or delete your Menus</p>
+            </div>
+            </section>
+
+            <footer>
+                <h2>Start Managing Your Corporate Food Today !</h2>
+                <hr />
+                <p>Get started with our Food Manager app</p>
+            </footer>
         </div>
     )
 
