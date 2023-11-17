10e22d6572deb9830e57490bc4798a527e11757b
Amir Mansour <swasat2009@hotmail.com>
2023-11-17 10:28:11 +0100

    catalog added

--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,10 +1,37 @@
-function Catalog () {
-    return(
-        <div>
-            <h1>CATALOG HERE</h1>
-        </div>
-    )
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Catalog.css"
 
+function Catalog() {
+  const [foods, setFoods] = useState([]);
+ 
+  const API_URL = "http://localhost:5005/food" 
+ 
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((response) => {
+        setFoods(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
+
+  return (
+    <div className='catalog-list'>
+      {foods.map((food) => (
+        <div key={food.id}>
+          <h1>{food.name}</h1>
+          <img src={food.image} alt={food.name} />
+          {/* <p>{food.instructions}</p> */}
+          <p>dish origin: {food.country}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Catalog
+export default Catalog;
