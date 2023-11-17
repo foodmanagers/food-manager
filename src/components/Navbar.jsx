c8ee89299d64cbffdf1085459cd4d018ac3c0a14
Hugo Pereira <hugocastroferro@gmail.com>
2023-11-17 09:35:48 +0000

    Add Navbar and Navbar basic CSS.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,19 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
 function Navbar () {
-    return(
-        <nav>
-            <h1>Navbar HERE</h1>
-        </nav>
+    return (
+        <nav className='Navbar'>
+      <NavLink to="/" className="NavIcon">
+      <img src="src\assets\home-icon.png" alt="Home Icon" />
+      </NavLink>
+      <NavLink to="/catalog" className="CatalogIcon">
+      <img src="src\assets\catalog.png" alt="Catalog Icon" />
+      </NavLink>
+      <NavLink to="/managemenus" className="ManagerIcon">
+      <img src="src\assets\manager.png" alt="Manager Icon" />
+      </NavLink>
+    </nav>
     )
 
 }
