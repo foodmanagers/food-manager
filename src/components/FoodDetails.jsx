af671216e477eb6e11862ddd5199f66a7ef164bd
Amir Mansour <swasat2009@hotmail.com>
2023-11-20 11:28:07 +0100

    delete functionality successfully added in the FoodDetails component

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,44 +1,49 @@
 import React from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-function FoodDetails(){
+function FoodDetails() {
+  const [foodDetails, setFoodDetails] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-const [foodDetails, setFoodDetails] = useState([])
-const {id} = useParams()
+  console.log(foodDetails);
 
-console.log(foodDetails);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5005/food/${id}`)
+      .then((response) => {
+        setFoodDetails(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
 
+  const deleteFunction = () => {
+    axios
+      .delete(`http://localhost:5005/food/${id}`)
+      .then(() => {
+        navigate("/foods"); //--------> the miracle
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-useEffect(() => {
-    axios.get(`http://localhost:5005/food/${id}`)
-    .then((response)=>{
-setFoodDetails(response.data)
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
-}, [id])
-
-
-return(
+  return (
     <div>
-    
-    <img src={foodDetails.image} alt={foodDetails.name} />
-    <p>Description: {foodDetails.instructions}</p>
-    <iframe width="420" height="315"
-src='https://www.youtube.com/embed/tgbNymZ7vqY'>
-</iframe>
-  </div>
-)
-
-
+      <img src={foodDetails.image} alt={foodDetails.name} />
+      <p>Description: {foodDetails.instructions}</p>
+      <iframe
+        width="420"
+        height="315"
+        src="https://www.youtube.com/embed/tgbNymZ7vqY"
+      ></iframe>
+      <button onClick={deleteFunction}>delete</button>
+    </div>
+  );
 }
-
-
-
-
-
 
 export default FoodDetails;
