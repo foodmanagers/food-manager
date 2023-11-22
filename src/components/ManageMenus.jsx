9b6b8ecaeddba4727bdb1c74a764875ec3c51391
Hugo Pereira <hugocastroferro@gmail.com>
2023-11-22 16:43:19 +0000

    [Fix] Manager Menu css

--- src/components/ManageMenus.jsx
+++ src/components/ManageMenus.jsx
@@ -25,14 +25,12 @@ function ManageMenus({admin}) {
   }, []);
 
   return (
-    <div>
+    <div className="pt-16">
       <>
-        <h1>Manager Menu</h1>
+        <h1 className="mb-10">Manager Menu</h1>
         {admin && (
           <Link to="/managemenus/create">
-            <p>
-              <button>Create</button>
-            </p>
+              <button className="mb-4 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mx-2" >Create</button>
           </Link>
         )}
         <hr />
