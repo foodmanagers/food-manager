90c459b01401ca3c1813d576b5a5128acd84f03c
Amir Mansour <swasat2009@hotmail.com>
2023-11-23 21:40:34 +0100

    items name in the cart and footer format

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,8 @@
-
-
-
 function Footer() {
   return (
     <div className="pt-6">
-      
-      <p>Copyright © 2023 - All right reserved by</p>
+      <p>Copyright © 2023 - All Rights Reserved</p>
+      <p>Food Managers, Inc.</p>
     </div>
   );
 }
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -7,6 +7,7 @@ function ShoppingCart({ cartItems, emptyCart }) {
       {cartItems.map((item) => (
         <div key={item.id} className=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <img className="rounded-t-lg"  src={item.image} alt={item.name} />
+          <p style={{  fontSize: '20px', fontWeight: 'bold' }}>{item.name}</p>
         </div>
       ))}
       
